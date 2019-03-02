807e99dd1c7b239638d91816ce95439889a5ba53
wangqidong <wangqidong@shuidihuzhu.com>
2019-03-02 17:48:46 +0800

    remark properties registry

--- src/main/java/com/bycsmys/ioc/BeanFactoryTest.java
+++ src/main/java/com/bycsmys/ioc/BeanFactoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.support.RootBeanDefinition;
 
 /**
  * 手动ioc注入
+ * from spring 揭秘 page 24
  *
  * @Author wangqd
  * @DATE 2019-03-02
@@ -18,9 +19,9 @@ import org.springframework.beans.factory.support.RootBeanDefinition;
 public class BeanFactoryTest {
 
     public static void main(String[] args) {
-        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        DefaultListableBeanFactory registry = new DefaultListableBeanFactory();
 
-        BeanFactory container = bindViaCode(beanFactory);
+        BeanFactory container = bindViaCode(registry);
 
         FxNewsProvider newsProvider = (FxNewsProvider) container.getBean("djNewsProvider");
         newsProvider.test();
@@ -32,20 +33,23 @@ public class BeanFactoryTest {
         AbstractBeanDefinition newsListener = new RootBeanDefinition(DowJonesNewListener.class);
         AbstractBeanDefinition newsPersister = new RootBeanDefinition(DowJonesNewsPersister.class);
 
+        //将bean对象注册到ioc容器
         registry.registerBeanDefinition("djNewsProvider", newProvider);
         registry.registerBeanDefinition("djListener", newsListener);
         registry.registerBeanDefinition("djPersister", newsPersister);
 
+        //构造方法注入
         ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
         argumentValues.addIndexedArgumentValue(0, newsListener);
         argumentValues.addIndexedArgumentValue(1, newsPersister);
 
         newProvider.setConstructorArgumentValues(argumentValues);
 
-        MutablePropertyValues propertyValues = new MutablePropertyValues();
-        propertyValues.addPropertyValue(new PropertyValue("newListener", newsListener));
-        propertyValues.addPropertyValue(new PropertyValue("newPersister", newsPersister));
-        newProvider.setPropertyValues(propertyValues);
+        //属性注入
+//        MutablePropertyValues propertyValues = new MutablePropertyValues();
+//        propertyValues.addPropertyValue(new PropertyValue("newListener", newsListener));
+//        propertyValues.addPropertyValue(new PropertyValue("newPersister", newsPersister));
+//        newProvider.setPropertyValues(propertyValues);
         return (BeanFactory) registry;
 
     }
